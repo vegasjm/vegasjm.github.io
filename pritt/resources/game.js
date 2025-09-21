048019f9607f977e03e172abdd232d1fca54ec9a
Jordi Munoz Vegas <jordi.munozvegas@betvictor.com>
2025-09-21 19:56:34 +0200

    worlds desings for diamonds game

--- pritt/resources/game.js
+++ pritt/resources/game.js
@@ -1,5 +1,6 @@
 var PRITT_GAME = PRITT_GAME || {
 	phase: 'world-selector',
+	world:1,
 	init: function(){
 		this.loadHome();
 		this.loadWorlds();
@@ -23,30 +24,30 @@ var PRITT_GAME = PRITT_GAME || {
 	loadWorlds: function() {
 		$('.btn-home-world').on('touch click', function(e){
 			PRITT_GAME.phase = 'game-1';
-			var world=$(this).attr('data-world');
+			PRITT_GAME.world=$(this).attr('data-world');
 			$('#legal').css('display','none');
 			setTimeout(function() {
 				$('.menu-home').css('display','none');
 				$('#game-intro').css('display','flex');
-				$('.game-character-img').attr("src",'./resources/img/world-'+world+'/world-'+world+'-character.png');
-				if(world == 2 || world == 3) {
+				$('.game-character-img').attr("src",'./resources/img/world-'+PRITT_GAME.world+'/world-'+PRITT_GAME.world+'-character.png');
+				if(PRITT_GAME.world == 2 || PRITT_GAME.world == 3) {
 					$('.game-character-img').css('float','right');
 						$('#game-wizard').css('transform','rotate(-28deg)');
 						$('#game-wizard').css('margin-left','-5rem');
-					if( world == 2) {
+					if( PRITT_GAME.world == 2) {
 						$('.body-inner').css('background-color','#F4E0B3');
 						$('.game-character-img').css('max-width','26vh');
 						$('#game-wizard-text').html('');
 						showText('#game-wizard-text','Oh no! @The rainbow on my @beach is gone!',0,75);
 					}
-					if( world == 3) {
+					if( PRITT_GAME.world == 3) {
 						$('.body-inner').css('background-color','#8652A9');
 						$('.game-character-img').css('max-width','28vh');
 						$('#game-wizard-text').html('');
 						showText('#game-wizard-text','I need your help to @make my cave @shine again!',0,75);
 					}
 				} else {
-					if(world == 1){
+					if(PRITT_GAME.world == 1){
 						$('.body-inner').css('background-color','#fbeda4');
 						$('#game-wizard').css('transform','rotate(0deg)');
 						$('#game-wizard').css('margin-left','-1rem');
@@ -56,13 +57,18 @@ var PRITT_GAME = PRITT_GAME || {
 						showText('#game-wizard-text','This forest is losing @its magic... @Will you help me?',0,75);
 					}
 				}
-				$('.body-inner').css('background-image','url(./resources/img/world-'+world+'/world-'+world+'-bg'+world+'.png)');
-				$('#body-overlay').css('background-image','url(./resources/img/world-'+world+'/world-'+world+'-bg'+world+'.png)');
+				$('.body-inner').css('background-image','url(./resources/img/world-'+PRITT_GAME.world+'/world-'+PRITT_GAME.world+'-bg1.png)');
+				$('#body-overlay').css('background-image','url(./resources/img/world-'+PRITT_GAME.world+'/world-'+PRITT_GAME.world+'-bg1.png)');
 			}, 500);
 		});
 	},
 	loadGamePhase1: function() {
 		$('#btn-start-game-phase1').on('touch click', function(e){
+			if( PRITT_GAME.world == 1) $('.body-inner').css('background-color','#E1D48D');
+			if( PRITT_GAME.world == 2) $('.body-inner').css('background-color','#B3E6DA');
+			if( PRITT_GAME.world == 3) $('.body-inner').css('background-color','#57319D');
+			$('.body-inner').css('background-image','url(./resources/img/world-'+PRITT_GAME.world+'/world-'+PRITT_GAME.world+'-bg2.png)');
+			$('#body-overlay').css('background-image','url(./resources/img/world-'+PRITT_GAME.world+'/world-'+PRITT_GAME.world+'-bg2.png)');
 			$('#game-intro').css('display','none');
 			$('#game-phase1').css('display','block');
 			$('#game-phase1-stats').css('display','flex');
@@ -93,7 +99,7 @@ var showText = function (target, message, index, interval) {
 var updateClock = function(time) {
 	$('#pendingSeconds').html(time);
 	if(time==0){
-		gameOver();
+
 	}else{
 		time-=1;
 		setTimeout("updateClock("+time+")",1000);
