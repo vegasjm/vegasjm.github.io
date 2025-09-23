e642d74a60b726c7ecad208864a1d5247a9d64a1
Jordi Munoz Vegas <jordi.munozvegas@betvictor.com>
2025-09-23 20:22:09 +0200

    increase game 1 seconds

--- pritt/resources/game.js
+++ pritt/resources/game.js
@@ -2,7 +2,7 @@ var PRITT_GAME = PRITT_GAME || {
     language: null,
     phase: 'world-selector',
     world: 1,
-    gameDuration: 1,
+    gameDuration: 90,
     gameRestarted: false,
     init: function() {
         this.loadLanguageSelector();
