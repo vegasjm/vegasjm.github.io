6193581fd07c411be04a992154a4c235f0eeb58b
vegasjm <jordi.munozvegas@bv-group.com>
2026-04-27 15:24:13 +0200

    templates pdf localization

--- pritt/resources/i18n.js
+++ pritt/resources/i18n.js
@@ -2,6 +2,16 @@ let translations = {};
 
 async function loadLanguage(lang) {
     const res = await fetch(`./resources/i18n/${lang}.json`);
+
+    $('#ForestKeeper-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-ForestKeeper.pdf');
+    $('#Unicorn-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-Unicorn.pdf');
+    $('#Ogre-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-Ogre.pdf');
+    $('#Dragon-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-Dragon.pdf');
+    $('#Fairy-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-Fairy.pdf');
+    $('#House-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-House.pdf');
+    $('#Rainbow-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-Rainbow.pdf');
+    $('#River-pdf').attr('href', 'resources/pdf/'+lang+'/Templates-River.pdf');
+
     translations = await res.json();
 
     document.querySelectorAll("[data-i18n]").forEach(el => {
